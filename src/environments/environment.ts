e9d0225f961b24f0de3784a69fb9e51949167173
Fernando Ramos <ferbologna08@gmail.com>
2023-09-07 20:16:34 -0300

    2

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'dtapp3-c1f9c',
+    appId: '1:258302262108:web:ddf29ad50ac52bc3511f99',
+    databaseURL: 'https://dtapp3-c1f9c-default-rtdb.firebaseio.com',
+    storageBucket: 'dtapp3-c1f9c.appspot.com',
+    apiKey: 'AIzaSyDsesk2of4hG9frxQ4GUrJ76IFoyOOJ-Zc',
+    authDomain: 'dtapp3-c1f9c.firebaseapp.com',
+    messagingSenderId: '258302262108',
+    measurementId: 'G-PVS5CZPP7D',
+  },
+};
